a982f2a25a2a56cdca867f8fdc03917359e6a88c
Pranav Gadge <gadgepranav32@gmail.com>
2026-07-23 17:49:59 +0530

    Implement initial user to UserDetails conversion

---
+++ src/main/java/com/internship/helpdesk/security/JwtUtils.java
@@ -0,0 +1,4 @@
+package com.internship.helpdesk.security;
+
+public class JwtUtils {
+}
---
+++ src/main/java/com/internship/helpdesk/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.internship.helpdesk.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+}
