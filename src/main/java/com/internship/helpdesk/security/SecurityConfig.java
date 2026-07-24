a43db66047f775ce1e82aa1e258fa65c47bf352b
Pranav Gadge <gadgepranav32@gmail.com>
2026-07-24 18:39:25 +0530

    Minor Bugs Fixed

--- src/main/java/com/internship/helpdesk/security/SecurityConfig.java
+++ src/main/java/com/internship/helpdesk/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
                 .authenticationProvider(authenticationProvider())
